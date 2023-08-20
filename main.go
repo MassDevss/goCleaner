0d842aa8797cc523cfb71bbbb018f0e94141e6bf
ManuelsSaNt <95022226+ManuelsSaNt@users.noreply.github.com>
2023-08-20 03:35:28 -0700

    basic program functions

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	f "fmt"
+	"io/fs"
 	"os"
 
 	h "github.com/ManuelsSaNt/goCleaner/helpers"
@@ -9,14 +10,24 @@ import (
 
 func main() {
 
-	f.Println("mamalon")
+	// var toDelete string = "node_modules"
 
-	readDir, err := os.ReadDir("../")
+	printDir("../")
 
+}
+
+func readADir(path string) []fs.DirEntry {
+	dir, err := os.ReadDir("../")
 	h.ManageErr(err)
 
-	for _, dirOrFile := range readDir {
+	return dir
+}
+
+func printDir(path string) {
+	dir, err := os.ReadDir("../")
+	h.ManageErr(err)
+
+	for _, dirOrFile := range dir {
 		f.Println(dirOrFile)
 	}
-
 }
