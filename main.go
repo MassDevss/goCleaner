2244b227c7ea2df585d7c408aded90f9b1ac9dfe
ManuelsSaNt <95022226+ManuelsSaNt@users.noreply.github.com>
2023-08-22 22:37:59 -0700

    actually can deletes but no uses the input by flag

--- main.go
+++ main.go
@@ -1,57 +1,100 @@
 package main
 
 import (
-	f "fmt"
+	"flag"
 	"io/fs"
 	"os"
+	"strings"
 
 	h "github.com/ManuelsSaNt/goCleaner/helpers"
+	"golang.org/x/exp/slices"
 )
 
 var dirToSwap string = ""
-var deppest int = 0
 
 func main() {
 
-	dirToClean := ".."
+	// ! config for flags
+	rootSearch := flag.String("root", "", "the initial dir for search the delete files.")
+	deleteFileOrDir := flag.String("target", "", "The files to delete (you can add more of one using ',')")
+	flag.Parse()
+
+	dirToClean := *rootSearch
 	dirToSwap = dirToClean
 
-	// var toDelete []string = []string{"node_modules"}
-	// baseDir := readADir(dirToClean)
+	// ! make split for get an array with the deletes targets
+	targets := strings.Split(*deleteFileOrDir, ",")
 
-	// recursiveCleaner(baseDir, toDelete)
+	// ! celaning the targets
+	for slices.Index(targets, " ") > -1 {
+		indexOfEmpty := slices.Index(targets, " ")
+		targets = slices.Delete(targets, indexOfEmpty, indexOfEmpty+1)
+	}
+
+	//! contains the clear targets to delete (use after the 'for' loop with this sign '#')
+	cleanTargets := []string{}
+
+	// !#
+	for _, target := range targets {
+		cleanTargets = append(cleanTargets, strings.Replace(target, " ", "", -1))
+	}
+	//! #
+
 	recursiveNavigator(dirToClean)
-
-	// f.Println()
-
 }
 
 func recursiveNavigator(setterPath string) {
 
 	baseDirCollection := readADir(setterPath)
-	var directories = []fs.DirEntry{}
 
 	for _, childDir := range baseDirCollection {
 
-		if childDir.IsDir() && childDir.Name() != "node_modules" {
-			directories = append(directories, childDir)
+		//! name of file or dir
+		elemName := childDir.Name()
+
+		if childDir.IsDir() {
+
+			//! path of file or dir
+			parentPath := setterPath + "/" + elemName
+
+			//! this is the perfect place to manage the files and dirs
+			//* write your logic here
+
+			// use for to loop the delete values
+			if strings.Contains(parentPath, "node_modules") {
+				splitedPath := strings.Split(parentPath, "/")
+
+				if splitedPath[len(splitedPath)-1] == "node_modules" {
+
+					delErr := os.RemoveAll(parentPath)
+
+					if delErr != nil {
+						// panic("An error ocurres deleting a file: " + delErr.Error())
+					}
+
+				}
+			}
+
+			//* write your logic here
+
+			// ! RECURSIVE
+			recursiveNavigator(parentPath)
 		}
 
-		f.Println(repeat(" ", deppest) + childDir.Name())
+		// ! this content here only shows you the files formated
+		// all this is only for my the code don't need it
+		// if childDir.IsDir() { //! in case to be directory
+		// 	f.Println("--------------------------------------------------")
+		// 	f.Println("DirName: " + childDir.Name())
+		// } else { //! in case to be a file with extension
+		// 	splitedFileName := strings.Split(childDir.Name(), ".")
+		// 	if len(splitedFileName) > 1 {
+		// 		f.Println("--------------------------------------------------")
+		// 		f.Println("FileName: " + splitedFileName[0] + "\next: " + splitedFileName[1])
+		// 	}
+		// }
+
 	}
-
-	// deppest++
-
-	for _, parent := range directories {
-
-		parentPath := setterPath + "/" + parent.Name()
-
-		f.Println("direcotry: " + parent.Name())
-		f.Println("path: " + parentPath)
-
-		recursiveNavigator(parentPath)
-	}
-
 }
 
 func readADir(path string) []fs.DirEntry {
@@ -59,23 +102,4 @@ func readADir(path string) []fs.DirEntry {
 	h.ManageErr(err)
 
 	return dir
-}
-
-func printDir(path string) {
-	dir, err := os.ReadDir(path)
-	h.ManageErr(err)
-
-	for _, dirOrFile := range dir {
-		f.Println(dirOrFile)
-	}
-}
-
-func repeat(toRepeat string, times int) string {
-	newTxt := ""
-
-	for i := 0; i < times; i++ {
-		newTxt += toRepeat
-	}
-
-	return newTxt
 }
